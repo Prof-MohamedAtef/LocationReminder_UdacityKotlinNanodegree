c8d36546990b6825a9a28917cb1a5b84a5188997
Mohamed Atef <proofesser@gmail.com>
2022-12-06 15:42:36 +0200

    Use FLAG_MUTABLE|App crashes|(Nexus 5X, API 32)|better solution

--- starter/app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
+++ starter/app/src/main/java/com/udacity/project4/utils/NotificationUtils.kt
@@ -37,8 +37,13 @@ fun sendNotification(context: Context, reminderDataItem: ReminderDataItem) {
     val stackBuilder = TaskStackBuilder.create(context)
         .addParentStack(ReminderDescriptionActivity::class.java)
         .addNextIntent(intent)
+
+    //todo:Strongly consider using FLAG_IMMUTABLE, only use FLAG_MUTABLE if some functionality depends on the PendingIntent being mutable
+    /*
+    todo solution : https://stackoverflow.com/a/67046334
+     */
     val notificationPendingIntent = stackBuilder
-        .getPendingIntent(getUniqueId(), PendingIntent.FLAG_UPDATE_CURRENT)
+        .getPendingIntent(getUniqueId(),PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
 //    build the notification object with the data to be shown
     val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
